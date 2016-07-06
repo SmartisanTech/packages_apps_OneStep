f98994217211e0515571d822797711885256434f
wangqianchuang <wangqianchuang@smartisan.com>
2016-07-06 15:18:53 +0930

    change the scroll area for DragScrollView
    Ticket: 115765
    
    Change-Id: I93ac2e6663d68af646f4a9ac04efc210ec3a4400
    Signed-off-by: wangqianchuang <wangqianchuang@smartisan.com>

--- src/com/smartisanos/sidebar/view/DragScrollView.java
+++ src/com/smartisanos/sidebar/view/DragScrollView.java
@@ -1,5 +1,6 @@
 package com.smartisanos.sidebar.view;
 
+import com.smartisanos.sidebar.R;
 import com.smartisanos.sidebar.util.ScrollController;
 
 import android.content.Context;
@@ -10,6 +11,9 @@ import android.widget.ScrollView;
 public class DragScrollView extends ScrollView {
 
     private ScrollController mScrollController;
+
+    private int mTopArea = 0;
+    private int mBottomArea = 0;
 
     public DragScrollView(Context context) {
         this(context, null);
@@ -22,6 +26,19 @@ public class DragScrollView extends ScrollView {
     public DragScrollView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
         mScrollController = new ScrollController(this);
+        mTopArea = getResources().getDimensionPixelSize(R.dimen.drag_scroll_view_top_area);
+        mBottomArea = getResources().getDimensionPixelSize(R.dimen.drag_scroll_view_bottom_area);
+    }
+
+    private boolean isScrollUp(){
+        return getScrollY() == 0;
+    }
+
+    private boolean isScrollBottom(){
+        if(getChildCount() <= 0){
+            return true;
+        }
+        return getChildAt(0).getMeasuredHeight() <= getScrollY() + getHeight();
     }
 
     @Override
@@ -30,10 +47,10 @@ public class DragScrollView extends ScrollView {
         switch (action) {
         case DragEvent.ACTION_DRAG_LOCATION:
             float y = event.getY();
-            if (y < 30) {
+            if (y < mTopArea && !isScrollUp()) {
                 mScrollController.scrollDown();
                 return true;
-            } else if (y > this.getHeight() - 200) {
+            } else if (y > this.getHeight() - mBottomArea && !isScrollBottom()) {
                 mScrollController.scrollUp();
                 return true;
             }
