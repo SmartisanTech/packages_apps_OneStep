1c3c6b700955a14c6864e15bf57ed7011b4c771f
lipeng <lipeng@smartisan.cn>
2016-07-12 15:11:02 +0800

    add StrictMode for sidebar
    Ticket: 0117795
    
    Change-Id: Ib4470d338c917ce670298b890f90d44bb9326784
    Signed-off-by: lipeng <lipeng@smartisan.cn>

--- src/com/smartisanos/sidebar/SidebarApplication.java
+++ src/com/smartisanos/sidebar/SidebarApplication.java
@@ -3,15 +3,40 @@ package com.smartisanos.sidebar;
 import com.smartisanos.sidebar.util.MailContactsHelper;
 
 import android.app.Application;
+import android.os.StrictMode;
 
 public class SidebarApplication extends Application {
+
+    private static final boolean ENABLE_STRICT_MODE = true;
 
     @Override
     public void onCreate() {
         super.onCreate();
+        setStrictMode();
         // this is necessary ! init it to make its inner data be filled
         // so we can use it correctly later
         MailContactsHelper.getInstance(this);
     }
 
+    private void setStrictMode() {
+        if (!ENABLE_STRICT_MODE) {
+            return;
+        }
+        StrictMode.ThreadPolicy threadPolicy = new StrictMode.ThreadPolicy.Builder()
+                .detectAll()
+                .detectDiskReads()
+                .detectDiskWrites()
+                .detectNetwork()
+                .detectCustomSlowCalls()
+                .build();
+        StrictMode.setThreadPolicy(threadPolicy);
+
+        StrictMode.VmPolicy vmPolicy = new StrictMode.VmPolicy.Builder()
+                .detectLeakedSqlLiteObjects()
+                .detectLeakedClosableObjects()
+                .penaltyLog()
+                .penaltyDeath()
+                .build();
+        StrictMode.setVmPolicy(vmPolicy);
+    }
 }
