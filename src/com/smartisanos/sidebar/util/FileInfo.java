10f3dec65da4c3ac2d405f3dc849e4c775bca530
wangqianchuang <wangqianchuang@smartisan.com>
2016-07-23 00:04:02 +0930

    Refine FileManager
    Ticket: 121726
    
    Change-Id: Ie99d8d54372e54a6e31d7f8c38db6f01ccbf256f
    Signed-off-by: wangqianchuang <wangqianchuang@smartisan.com>

--- src/com/smartisanos/sidebar/util/FileInfo.java
+++ src/com/smartisanos/sidebar/util/FileInfo.java
@@ -2,64 +2,135 @@ package com.smartisanos.sidebar.util;
 
 import android.os.Environment;
 import android.text.TextUtils;
+import android.util.Log;
+import android.webkit.MimeTypeMap;
 
 import java.io.File;
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Set;
 
 public class FileInfo {
-    public String filePath = "";
-    public String mimeType = null;
-    public int id = 0;
-    public String title;
-    public long time = 0;
-    public int size = 0;
-    public String pathID;
-    private final String log = "log";
     public static final String[] MIMETYPE_BLACKLIST = new String[] { "image/*" };
 
-    private static final String[] mFilePathBlackList = new String[]{
+    private static final String[] BLACKLIST = new String[]{
         Environment.getExternalStorageDirectory().getAbsolutePath()+"/OpenMaster/plugins/",
-        Environment.getExternalStorageDirectory().getAbsolutePath()+"/tencent/MobileQQ/.apollo/role/",
-        Environment.getExternalStorageDirectory().getAbsolutePath()+"/tencent/MobileQQ/qbiz/html5/",
-        Environment.getExternalStorageDirectory().getAbsolutePath()+"/tencent/MobileQQ/PhotoPlus/",
-        Environment.getExternalStorageDirectory().getAbsolutePath()+"/tencent/MobileQQ/ar_model/"
+        Environment.getExternalStorageDirectory().getAbsolutePath()+"/tencent/",
+        Environment.getExternalStorageDirectory().getAbsolutePath()+"/Tencent/"
     };
 
-    public boolean valid() {
-        if (TextUtils.isEmpty(filePath) || TextUtils.isEmpty(mimeType)) {
-            return false;
-        }
+    private static final String[] WHITELIST = new String[]{
+        Environment.getExternalStorageDirectory().getAbsolutePath()+"/tencent/QQfile_recv/",
+        Environment.getExternalStorageDirectory().getAbsolutePath()+"/tencent/MicroMsg/Download/",
+        Environment.getExternalStorageDirectory().getAbsolutePath()+"/Tencent/QQfile_recv/",
+        Environment.getExternalStorageDirectory().getAbsolutePath()+"/Tencent/MicroMsg/Download/"
+    };
 
-        File file = new File(filePath);
-        if (!file.isFile() || !file.exists()) {
-            return false;
-        }
+    private static final Set<String> SUFFIXSET;
+    private static final Set<String> PATH_MASK;
 
-        title = file.getName();
-        if(title.toLowerCase().contains(log) || (size == 0) ){
-            return false;
-        }
+    static {
+        SUFFIXSET = new HashSet<String>();
+        SUFFIXSET.add("xlsx");
+        SUFFIXSET.add("key");
+        SUFFIXSET.add("pptx");
+        SUFFIXSET.add("numbers");
+        SUFFIXSET.add("rar");
+        SUFFIXSET.add("apk");
+        SUFFIXSET.add("7z");
+        SUFFIXSET.add("docx");
+        SUFFIXSET.add("pages");
+        PATH_MASK = new HashSet<String>();
+        PATH_MASK.add("backup");
+        PATH_MASK.add("crash");
+    }
 
-        for(int i = 0; i <mFilePathBlackList.length ; i++ ){
-            if(filePath.contains(mFilePathBlackList[i])){
-                return false;
-            }
-        }
+    public String filePath = "";
+    public String mimeType;
+    public long time = 0;
+    public String hashKey;
 
-        time = file.lastModified();
-        pathID = filePath + time;
-        return true;
+    public FileInfo(String path){
+        this(path, getFileMimeType(new File(path)));
+    }
+
+    public FileInfo(String path, String mimeType){
+        filePath = path;
+        this.mimeType =mimeType;
+        time = new File(path).lastModified();
+        hashKey = path + time;
     }
 
     public int getIconId() {
-        String suffix = null;
         String name = new File(filePath).getName();
-        if (!TextUtils.isEmpty(name)) {
-            String[] sp = name.split("\\.");
-            if (sp.length > 1) {
-                suffix = sp[sp.length - 1];
-            }
-        }
-        return MimeUtils.getResId(mimeType, suffix);
+        return MimeUtils.getResId(mimeType, getSuffix(name));
     }
 
+    public boolean valid() {
+        if (TextUtils.isEmpty(filePath)) {
+            return false;
+        }
+
+        if (!isMimeTypeAndFilePathValid(mimeType, filePath)) {
+            return false;
+        }
+
+        for (int i = 0; i < BLACKLIST.length; i++) {
+            if (filePath.startsWith(BLACKLIST[i])) {
+                boolean ok = false;
+                for (int j = 0; j < WHITELIST.length; j++) {
+                    if (filePath.startsWith(WHITELIST[j])) {
+                        ok = true;
+                        break;
+                    }
+                }
+                if (!ok) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private static boolean isMaskFile(File file){
+        if(file == null){
+            return false;
+        }
+
+        String name = file.getName();
+        if(name.startsWith(".") || PATH_MASK.contains(name.toLowerCase())){
+            return true;
+        }
+        return isMaskFile(file.getParentFile());
+    }
+
+    public static boolean isMimeTypeAndFilePathValid(String mimeType, String filePath) {
+        File file = new File(filePath);
+        if (!file.isFile() || isMaskFile(file.getParentFile())) {
+            return false;
+        }
+        if (file.getName().toLowerCase().contains("log")) {
+            return false;
+        }
+        if (TextUtils.isEmpty(mimeType)) {
+            if (!SUFFIXSET.contains(getSuffix(file.getName()))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static String getSuffix(String fileName) {
+        int index = fileName.lastIndexOf(".");
+        if (index != -1) {
+            return fileName.substring(index + 1).toLowerCase(Locale.US);
+        } else {
+            return "";
+        }
+    }
+
+    public static String getFileMimeType(File file){
+        String suffix = getSuffix(file.getName());
+        return MimeTypeMap.getSingleton().getMimeTypeFromExtension(suffix);
+    }
 }
--- src/com/smartisanos/sidebar/util/RecentFileManager.java
+++ src/com/smartisanos/sidebar/util/RecentFileManager.java
@@ -5,6 +5,7 @@ import android.database.ContentObserver;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Environment;
+import android.os.FileObserver;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.Looper;
@@ -13,7 +14,7 @@ import android.provider.MediaStore.Audio;
 import android.provider.MediaStore.Files;
 import android.provider.MediaStore.Files.FileColumns;
 import android.provider.MediaStore.Video;
-import android.webkit.MimeTypeMap;
+import android.util.Log;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -21,10 +22,11 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Locale;
 import java.util.Set;
 
 public class RecentFileManager extends DataManager implements IClear{
+
+    private static final String TAG = RecentFileManager.class.getName();
 
     private volatile static RecentFileManager sInstance;
     public synchronized static RecentFileManager getInstance(Context context){
@@ -50,7 +52,7 @@ public class RecentFileManager extends DataManager implements IClear{
     private Context mContext;
     private Handler mHandler;
 
-    private static final int MSG_UPDATE_FILE_LIST = 0;
+    private static final int MSG_UPDATE_DATABASE_LIST = 0;
     private static final int MSG_SEARCH_FILE = 1;
 
     private static final String VOLUME_EXTERNAL = "external";
@@ -64,34 +66,28 @@ public class RecentFileManager extends DataManager implements IClear{
     private final Uri musicUri = Audio.Media.getContentUri(VOLUME_EXTERNAL);
     private final Uri videoUri = Video.Media.getContentUri(VOLUME_EXTERNAL);
 
-    private final String QQFilePath = Environment.getExternalStorageDirectory().getAbsolutePath()+"/tencent/QQfile_recv/";
-    private final String MsgFilePath = Environment.getExternalStorageDirectory().getAbsolutePath()+"/tencent/MicroMsg/Download/";
-    private final String DingFilePath = Environment.getExternalStorageDirectory().getAbsolutePath()+"/DingTalk/";
-
-    private File mQQFile = new File(QQFilePath);
-    private File mMsgFile = new File(MsgFilePath);
-    private File mDingFile = new File(DingFilePath);
+    private static final String[] TARGET_DIR = new String[]{
+        Environment.getExternalStorageDirectory().getAbsolutePath()+"/tencent/QQfile_recv/",
+        Environment.getExternalStorageDirectory().getAbsolutePath()+"/tencent/MicroMsg/Download/",
+        Environment.getExternalStorageDirectory().getAbsolutePath()+"/Tencent/QQfile_recv/",
+        Environment.getExternalStorageDirectory().getAbsolutePath()+"/Tencent/MicroMsg/Download/",
+        Environment.getExternalStorageDirectory().getAbsolutePath()+"/DingTalk/"
+    };
 
     private List<FileInfo> mList = new ArrayList<FileInfo>();
-
     private List<FileInfo> mCursorCacheList = new ArrayList<FileInfo>();
-    private Set<String> mCursorCacheSet = new HashSet<String>();
-
     private List<FileInfo> mSearchCacheList = new ArrayList<FileInfo>();
-    private List<String> mSearchCachePathList = new ArrayList<String>();
 
-    private boolean mAddFile = false;
-    private FileObserver mFileObserver;
+    private DatabaseObserver mDatabaseObserver;
     private FileClearDatabaseHelper mDatabaseHelper;
 
     private RecentFileManager(Context context) {
         mContext = context;
-        mFileObserver =  new FileObserver(mHandler);
+        mDatabaseObserver =  new DatabaseObserver(mHandler);
         mDatabaseHelper = new FileClearDatabaseHelper(mContext);
         HandlerThread thread = new HandlerThread(RecentPhotoManager.class.getName());
         thread.start();
         mHandler = new FileManagerHandler(thread.getLooper());
-        mDatabaseHelper.initFileClearHelper(mHandler);
     }
 
     public List<FileInfo> getFileList(){
@@ -110,90 +106,94 @@ public class RecentFileManager extends DataManager implements IClear{
     }
 
     public void startFileObserver(){
-        mContext.getContentResolver().registerContentObserver(fileUri, true, mFileObserver);
-        mContext.getContentResolver().registerContentObserver(musicUri, true, mFileObserver);
-        mContext.getContentResolver().registerContentObserver(videoUri, true, mFileObserver);
-        mHandler.obtainMessage(MSG_UPDATE_FILE_LIST).sendToTarget();
+        mContext.getContentResolver().registerContentObserver(fileUri, true, mDatabaseObserver);
+        mContext.getContentResolver().registerContentObserver(musicUri, true, mDatabaseObserver);
+        mContext.getContentResolver().registerContentObserver(videoUri, true, mDatabaseObserver);
+        mHandler.obtainMessage(MSG_UPDATE_DATABASE_LIST).sendToTarget();
     }
 
     public void stopFileObserver() {
         if (mHandler.hasMessages(MSG_SEARCH_FILE)) {
             mHandler.removeMessages(MSG_SEARCH_FILE);
         }
-        mContext.getContentResolver().unregisterContentObserver(mFileObserver);
+        mContext.getContentResolver().unregisterContentObserver(mDatabaseObserver);
     }
 
-    private void searchFile(){
-        mAddFile = false;
-        boolean deleteFile = false;
-        List<String> qqList = searchDestinationFolder(mQQFile);
-        List<String> msgList = searchDestinationFolder(mMsgFile);
-        List<String> dingList = searchDestinationFolder(mDingFile);
-        updateFilePath(qqList);
-        updateFilePath(msgList);
-        updateFilePath(dingList);
-        for(int i = 0;i < mSearchCachePathList.size() ; i++){
-            File file = new File(mSearchCachePathList.get(i));
-            if(!file.exists()){
-                deleteFile = true;
-            }
-        }
-        if(deleteFile || mAddFile){
-            sortRecentFileList();
-        }
-    }
-
-    private void updateListContent(){
-        mCursorCacheList.clear();
-        mCursorCacheSet.clear();
-        setListDataFromCursor(getContentCursor(MUSIC_TYPE));
-        setListDataFromCursor(getContentCursor(VIDEO_TYPE));
-        setListDataFromCursor(getContentCursor(FILE_TYPE));
+    public void onClearSetChange(){
         sortRecentFileList();
     }
 
-    private void updateFilePath(List<String> list){
-        for(int i = 0 ; i < list.size() ; i++){
-            String filePath = list.get(i);
-            File file = new File(filePath);
-            String fileName = file.getName();
-            String fileTitle = fileName.substring(0,fileName.lastIndexOf("."));
-            String mimeType = getFileMimeType(file);
-            Long time = file.lastModified();
-            String filePathID = filePath + time ;
-            if( !fileTitle.equals("") && !mSearchCachePathList.contains(filePath) && mimeType != null ){
-                FileInfo info = new FileInfo();
-                info.filePath = filePath;
-                info.mimeType = mimeType;
-                info.time = time;
-                info.pathID = filePathID;
-                mSearchCacheList.add(info);
-                mSearchCachePathList.add(filePath);
-                mAddFile = true;
-            }
-        }
-    }
-
-    private void sortRecentFileList(){
-        Set<String> clearSet = mDatabaseHelper.getClearSet();
-        for(int i = 0 ; i < mSearchCacheList.size() ; i++){
-            if( !clearSet.contains(mSearchCacheList.get(i).pathID) ){
-                File file = new File(mSearchCachePathList.get(i));
-                if( file.exists() && !mCursorCacheSet.contains(mSearchCachePathList.get(i)) ){
-                    mCursorCacheList.add(mSearchCacheList.get(i));
-                    mCursorCacheSet.add(mSearchCacheList.get(i).filePath);
+    private List<String> searchDestinationFolder(File dir) {
+        List<String> filePathList = new ArrayList<String>();
+        if (dir.exists()) {
+            for (File file : dir.listFiles()) {
+                if (file.isFile()) {
+                    filePathList.add(file.getAbsolutePath());
                 }
             }
         }
+        return filePathList;
+    }
+
+    private void searchFile(){
+        List<String> allFile = new ArrayList<String>();
+        for(String path : TARGET_DIR){
+            allFile.addAll(searchDestinationFolder(new File(path)));
+        }
+        for(int i = 0 ; i < allFile.size() ; i++){
+            String filePath = allFile.get(i);
+            FileInfo info = new FileInfo(filePath);
+            if(info.valid()){
+                mSearchCacheList.add(info);
+            }
+        }
+        if(false){
+            for(FileInfo info : mSearchCacheList){
+                Log.d(TAG, "sdcard.fileinfo -> " + info.filePath);
+            }
+        }
+    }
+
+    private void updateDatabaseContent(){
+        mCursorCacheList.clear();
+        mCursorCacheList.addAll(getFileInfoByCursor(getContentCursor(MUSIC_TYPE)));
+        mCursorCacheList.addAll(getFileInfoByCursor(getContentCursor(VIDEO_TYPE)));
+        mCursorCacheList.addAll(getFileInfoByCursor(getContentCursor(FILE_TYPE)));
+        if(false){
+            for(FileInfo info : mCursorCacheList){
+                Log.d(TAG, "database.fileinfo -> " + info.filePath);
+            }
+        }
+    }
+
+    private void sortRecentFileList() {
+        List<FileInfo> allInfo = new ArrayList<FileInfo>();
+        Set<String> clearSet = mDatabaseHelper.getClearSet();
+        Set<String> dataSet = new HashSet<String>();
+        for (FileInfo info : mCursorCacheList) {
+            if (!clearSet.contains(info.hashKey)
+                    && !dataSet.contains(info.filePath)) {
+                dataSet.add(info.filePath);
+                allInfo.add(info);
+            }
+        }
+
+        for (FileInfo info : mSearchCacheList) {
+            if (!clearSet.contains(info.hashKey)
+                    && !dataSet.contains(info.filePath)) {
+                dataSet.add(info.filePath);
+                allInfo.add(info);
+            }
+        }
+
         FileComparator comparator = new FileComparator();
-        Collections.sort(mCursorCacheList,comparator);
+        Collections.sort(allInfo, comparator);
         synchronized (RecentFileManager.class) {
             mList.clear();
-            mList.addAll(mCursorCacheList);
+            mList.addAll(allInfo);
         }
         notifyListener();
     }
-
 
     private class FileComparator implements Comparator<FileInfo> {
         public int compare(FileInfo fileInfo1, FileInfo fileInfo2) {
@@ -208,26 +208,34 @@ public class RecentFileManager extends DataManager implements IClear{
         }
     }
 
-    private void setListDataFromCursor(Cursor cursor){
+    private List<FileInfo> getFileInfoByCursor(Cursor cursor) {
+        List<FileInfo> infos = new ArrayList<FileInfo>();
         if (cursor != null) {
-            Set<String> clearSet = mDatabaseHelper.getClearSet();
-            if (cursor.moveToFirst()) {
-                do {
-                    FileInfo info = new FileInfo();
-                    info.filePath = cursor.getString(cursor.getColumnIndexOrThrow(FileColumns.DATA));
-                    info.mimeType = cursor.getString(cursor.getColumnIndexOrThrow(FileColumns.MIME_TYPE));
-                    info.id = cursor.getInt(cursor.getColumnIndexOrThrow(FileColumns._ID));
-                    info.size = cursor.getInt(cursor.getColumnIndexOrThrow(FileColumns.SIZE));
-                    if (info.valid()) {
-                        if(!clearSet.contains(info.pathID)){
-                            mCursorCacheList.add(info);
-                            mCursorCacheSet.add(info.filePath);
+            try {
+                Set<String> clearSet = mDatabaseHelper.getClearSet();
+                if (cursor.moveToFirst()) {
+                    do {
+                        int size = cursor.getInt(cursor.getColumnIndexOrThrow(FileColumns.SIZE));
+                        if (size == 0) {
+                            continue;
                         }
-                    }
-                } while (cursor.moveToNext());
+                        String filePath = cursor.getString(cursor.getColumnIndexOrThrow(FileColumns.DATA));
+                        String mimeType = cursor.getString(cursor.getColumnIndexOrThrow(FileColumns.MIME_TYPE));
+                        FileInfo info = new FileInfo(filePath, mimeType);
+                        if (info.valid()) {
+                            if (!clearSet.contains(info.hashKey)) {
+                                infos.add(info);
+                            }
+                        }
+                    } while (cursor.moveToNext());
+                }
+            } catch(Exception e){
+                    // NA
+            } finally {
+                cursor.close();
             }
-            cursor.close();
         }
+        return infos;
     }
 
     private Cursor getContentCursor(int type){
@@ -242,63 +250,26 @@ public class RecentFileManager extends DataManager implements IClear{
         }
     }
 
-    private class FileObserver extends ContentObserver{
-        public FileObserver(Handler handler) {
+    private class ReceiveFileOberver extends FileObserver{
+        public ReceiveFileOberver(String path) {
+            super(path);
+        }
+
+        @Override
+        public void onEvent(int event, String path) {
+            // NA
+        }
+    }
+
+    private class DatabaseObserver extends ContentObserver{
+        public DatabaseObserver(Handler handler) {
             super(handler);
         }
 
         @Override
         public void onChange(boolean selfChange) {
             super.onChange(selfChange);
-            mHandler.obtainMessage(MSG_UPDATE_FILE_LIST).sendToTarget();
-        }
-    }
-
-    @Override
-    public void clear() {
-        synchronized (RecentFileManager.class) {
-            mDatabaseHelper.insertTableData(mList);
-            mHandler.obtainMessage(MSG_UPDATE_FILE_LIST).sendToTarget();
-        }
-    }
-
-    private List<String> searchDestinationFolder(File file){
-        List<String> filePathList = new ArrayList<String>();
-        if(file.exists()){
-            for(File nfile : file.listFiles()){
-                if( nfile.exists() && nfile.isFile() ){
-                    filePathList.add(nfile.getAbsolutePath());
-                }
-            }
-        }
-        return filePathList;
-    }
-
-    private String getFileMimeType(File file){
-        String suffix = getSuffix(file);
-        if (suffix == null) {
-            return null;
-        }
-        String mimeType = MimeTypeMap.getSingleton().getMimeTypeFromExtension(suffix);
-        if (mimeType == null || mimeType.isEmpty()) {
-            return null;
-        }
-        return mimeType;
-    }
-
-    private String getSuffix(File file) {
-        if (file == null || !file.exists() || file.isDirectory()) {
-            return null;
-        }
-        String fileName = file.getName();
-        if (fileName.equals("") || fileName.endsWith(".")) {
-            return null;
-        }
-        int index = fileName.lastIndexOf(".");
-        if (index != -1) {
-            return fileName.substring(index + 1).toLowerCase(Locale.US);
-        } else {
-            return null;
+            mHandler.obtainMessage(MSG_UPDATE_DATABASE_LIST).sendToTarget();
         }
     }
 
@@ -311,14 +282,23 @@ public class RecentFileManager extends DataManager implements IClear{
         @Override
         public void handleMessage(Message msg) {
             switch (msg.what) {
-                case MSG_UPDATE_FILE_LIST:
-                    updateListContent();
+                case MSG_UPDATE_DATABASE_LIST:
+                    updateDatabaseContent();
+                    sortRecentFileList();
                     break;
                 case MSG_SEARCH_FILE:
                     searchFile();
+                    sortRecentFileList();
                     break;
             }
         }
     }
 
+    @Override
+    public void clear() {
+        synchronized (RecentFileManager.class) {
+            mDatabaseHelper.insertTableData(mList);
+            mList.clear();
+        }
+    }
 }
